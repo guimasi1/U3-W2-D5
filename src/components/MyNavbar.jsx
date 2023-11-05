62d78ddfb6b91433c20720b24b184eea9dfb614c
guimasi1 <143355576+guimasi1@users.noreply.github.com>
2023-11-05 21:33:14 +0100

    link to homepage

--- src/components/MyNavbar.jsx
+++ src/components/MyNavbar.jsx
@@ -8,7 +8,9 @@ const MyNavbar = () => {
     <Navbar expand="lg" className="bg-blue-dark sticky-top ">
       <Container>
         <Navbar.Brand className="text-white" href="#home">
-          WeatherNow
+          <Link className="text-decoration-none text-white" to="/">
+            WeatherNow
+          </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
